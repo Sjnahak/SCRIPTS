525174d9bdc28432986fbd61a9ce12e7fa283ae6
suraj <suraj.nahak@brillio.com>
2021-04-29 16:44:34 +0530

    cleanup script

--- Bash/individualrepo_cleanup.sh
+++ Bash/individualrepo_cleanup.sh
@@ -8,3 +8,54 @@ for i in {25..98} ; do
 curl -X DELETE -u $jfrog_user:$jfrog_pass https://companyname.jfrog.io/artifactory/$repo/api-$i-1.0.0-SNAPSHOT-mule-application.jar
 curl -X DELETE -u $jfrog_user:$jfrog_pass https://companyname.jfrog.io/artifactory/api/trash/clean/$repo/api-$i-1.0.0-SNAPSHOT-mule-application.jar
 done
+
+#Search a pattern and delete it
+#!/bin/bash
+jfrog_user=""
+jfrog_pass=""
+repo=reponame
+
+for i in {371..373} ; do
+patterns=`curl -s -u  $jfrog_user:$jfrog_pass https://companyname.jfrog.io/artifactory/api/search/pattern?pattern=$repo:folder/HtmlReport_apiname_$i*.zip | jq -r '.files[]?'`
+echo $patterns
+done
+#curl -X DELETE -u $jfrog_user:$jfrog_pass https://companyname.jfrog.io/artifactory/$repo/$patterns
+
+
+###########################################################################################################################################################
+#!/bin/bash
+CURRENT_TIME=`date +%s%3N`
+echo "enter build name?"
+read BUILDS_NAME
+echo "Enter repo name?"
+read REPO_NAME
+echo "Enter tag devartifact or qaartifact name?"
+read artifact_tag
+echo "Enter tag snapshot or qaartifact name?"
+read artifact_value
+jfrog_user=""
+echo "enter jfrog password"
+read jfrog_pass
+# FETCHING URL DETAIS FOR MATCHING PROPERTY NAME Env property repos, Fetcth Corresponding builds to delete using the usl
+untagged_jar=`curl -s -u $jfrog_user:$jfrog_pass "https://comapnyname.jfrog.io/artifactory/api/search/prop?$artifact_tag=$artifact_value&repos=$REPO_NAME" | jq -r '.results[]?.uri'`
+#untagged_jar=`curl -s -u $jfrog_user:$jfrog_pass "https://companyname.jfrog.io/artifactory/api/search/prop?env=dev&repos=companyname-api-dev" | jq -r '.results[]?.uri'| head -n -3`
+
+for build in $BUILDS_NAME ; do
+   if [ -z "$untagged_jar" ]
+   then
+      echo "#####NO Snapshot BUILD FOUND FOR $build NOTHING TO DELETE HENCE MOVING TO NEXT REPO#####"
+   else
+      for build_info in $untagged_jar; do
+        snapshot_build=`curl -s -u $jfrog_user:$jfrog_pass $build_info?properties=build.number  | jq -r '.properties[]?[]'`
+        echo "#####SNAPHOT BUILD NUMBER FOUND FOR $build HENCE PROCEEDING WITH DELETE LOOP#####"
+        echo "Your build number : $snapshot_build"
+        PATH_TO_ARTIFACT=`curl -s -X GET -u $jfrog_user:$jfrog_pass $build_info | jq -r '.downloadUri'`
+        echo $PATH_TO_ARTIFACT
+        #curl -X DELETE -u $jfrog_user:$jfrog_pass "https://comapnyname.jfrog.io/artifactory/api/build/$build?buildNumbers=$snapshot_build"
+        #curl -X DELETE -u $jfrog_user:$jfrog_pass "$PATH_TO_ARTIFACT"
+      done
+    fi
+done
+
+#head -n -1   removes 1 line from bottom
+#tail -n +2   removes 1 line lesser than the provided number from top(example if given 2 it will remove 1)
--- begin-python/dockerimages_cleanup.py
+++ begin-python/dockerimages_cleanup.py
@@ -7,21 +7,27 @@ def clean_docker():
     headers = {
         'content-type': 'text/plain',
     }
-
+    username = ''
+	password = ''
     #Returns Docker Images for all repo
     #data = 'items.find({"name":{"$eq":"manifest.json"},"stat.downloaded":{"$before":"4w"}})'
 	
     #Returns Dockerimages for Specific repo and for the required build number
 	#Range can taken as user input and passed instead of hard cording
     for x in range(63, 65):
-        data = 'items.find({"repo" : "companyname-api-name-dev"},{"name":{"$eq":"manifest.json"}},{"artifact.module.build.number":{"$eq":"%s"}})' % (x)
+        data = 'items.find({"repo" : "companyname-api-name-dev"},{"name":{"$eq":"manifest.json"}},{"artifact.module.build.name":{"$eq":"%s"}},{"artifact.module.build.number":{"$eq":"%s"}})' % (x)
+		#data = 'items.find({"repo" : "reponame"},{"name":{"$eq":"manifest.json"}},{"artifact.module.build.name":{"$eq":"demoname"}},{"artifact.module.build.number":{"$eq":"9"}})'
 
-        myResp = requests.post(base_url+'api/search/aql', auth=('user', 'passowrd'), headers=headers, data=data)
+        myResp = requests.post(base_url+'api/search/aql', auth=('username', 'password'), headers=headers, data=data)
 
         for result in eval(myResp.text)["results"]:
             artifact_url = base_url+ result['repo'] + '/' + result['path']
 			print(artifact_url)
-			requests.delete(artifact_url, auth=('user', 'passowrd'))
+			requests.delete(artifact_url, auth=('username', 'password'))
+			build_url = base_url+'api/build/buildname?buildNumbers=%s' % (x)
+			print(build_url)
+			requests.delete(build_url, auth=('username', 'password'))
+			
 
 if __name__ == '__main__':
     clean_docker()
