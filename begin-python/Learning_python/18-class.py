956ba7a9e0b8161615cbc2e2d732dcbbba6c4fbc
Sjnahak <33506659+Sjnahak@users.noreply.github.com>
2024-01-05 08:31:11 +0000

    Update 18-class.py

--- begin-python/Learning_python/18-class.py
+++ begin-python/Learning_python/18-class.py
@@ -1,4 +1,5 @@
-####Jus like append,remove etc in list we can make class
+####Jus like append,remove etc in list we can make class 
+#https://docs.python.org/3/tutorial/classes.html#classes
 #Class start with a camel case
 #self is instance of class
 #Methods are functions inside a class
@@ -6,21 +7,31 @@
 #def __int__ (constructor of the class)
 #attributes can be accessed as self.attribute
 
-class Point:
-    def move(self):
-        print("move")
-        
-    def draw(self):
-        print("draw")
+class Vehicle:
+    """
+    Vehicle models a vehicle w/ tires and an engine
+    """
+
+    def __init__(self, engine, tires):
+        self.engine = engine
+        self.tires = tires
+
+    #Custom Method
+    def description(self):
+        print(f"A vehicle with an {self.engine} engine, and {self.tires} tires")
 
 ##create object
-point1 = Point()
-point1.draw()
+>>> civic = Vehicle('4-cylinder', ['front-driver', 'front-passenger', 'rear-driver', 'rear-passenger'])
+>>> civic.engine
+'4-cylinder'
 
+>>> civic.tires
+['front-driver', 'front-passenger', 'rear-driver', 'rear-passenger']
 
-#####pass attribute to a class
-point1.x = 10
-point1.y = 20
-print(point1.x)
+>>> civic.description
+<bound method Vehicle.description of <__main__.Vehicle object at 0x7fb5f3fbbda0>>
+
+>>> civic.description()
+A vehicle with a 4-cylinder engine, and ['front-driver', 'front-passenger', 'rear-driver', 'rear-passenger'] tires
 
 
